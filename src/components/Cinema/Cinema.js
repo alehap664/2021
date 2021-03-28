7987b2f72329662ab34b71ffb2bb42065e0befac
Nguyễn Thịnh <nguyenducthinh9a10@gmail.com>
2021-03-28 13:46:19 +0700

    ahihi

--- src/components/Cinema/Cinema.js
+++ src/components/Cinema/Cinema.js
@@ -1,18 +1,19 @@
-import {BrowserRouter as Router, Switch, Route, useRouteMatch} from 'react-router-dom'
+import { Switch, Route, useRouteMatch} from 'react-router-dom'
 import Nav from './Nav/Nav';
 import Home from './Home/Home';
 import Detail from './Detail/Detail';
 
 function Cinema() {
   const { path } = useRouteMatch();
+  console.log(path);
   return (
-    <Router>
+    <>
       <Nav></Nav>
       <Switch>
         <Route path={path} exact component={Home} />
         <Route path={`${path}/detail/:id`} exact component={Detail} />
       </Switch>
-    </Router>
+    </>
   );
 }
 
