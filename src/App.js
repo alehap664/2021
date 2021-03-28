00180326d5d133cbc658c2494a18b34f852e150e
Nguyễn Thịnh <nguyenducthinh9a10@gmail.com>
2021-03-28 13:03:11 +0700

    ahihi

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import Admin from './components/Admin/Admin';
 
 function App() {
   return (
-    // <Router>
-    //   <Switch>
-    //     <Route path="/" exact component={Home} />
-    //     <Route path="/cinema" component={Cinema} />
-    //     <Route path="/admin" component={Admin} />
-    //   </Switch>
-    // </Router>
-    <Cinema />
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/cinema" component={Cinema} />
+        <Route path="/admin" component={Admin} />
+      </Switch>
+    </Router>
   );
 }
 
