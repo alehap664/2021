7f62956021be583ea60bfdb86a74afb963d4280c
Nguyễn Thịnh <nguyenducthinh9a10@gmail.com>
2021-03-28 11:46:24 +0700

    None

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 import Cinema from './components/Cinema/Cinema';
 import Admin from './components/Admin/Admin';
 
@@ -7,11 +7,18 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/cinema" component={Cinema} />
-        <Route path="/admin" component={Admin} />
+        <Route path="https://alehap664.github.io/react-cinema/" exact component={Home} />
+        <Route path="https://alehap664.github.io/react-cinema/cinema" component={Cinema} />
+        <Route path="https://alehap664.github.io/react-cinema/admin" component={Admin} />
       </Switch>
     </Router>
   );
+}
+
+const Home = () => {
+  return(
+    <Redirect to="/cinema" />
+  )
 }
 
 export default App;
